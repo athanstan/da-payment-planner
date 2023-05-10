3c0f13b65d8264df00ae82f015e91a006995190f
athanstan <stantzouris.thanos@gmail.com>
2023-05-10 15:56:23 +0300

    [feature] Add Filters for active and email. No Filters mean show all data

--- app/Commands/ReadExcelCommand.php
+++ app/Commands/ReadExcelCommand.php
@@ -4,6 +4,7 @@ namespace App\Commands;
 
 use Illuminate\Console\Scheduling\Schedule;
 use LaravelZero\Framework\Commands\Command;
+use Spatie\SimpleExcel\SimpleExcelReader;
 
 class ReadExcelCommand extends Command
 {
@@ -12,14 +13,14 @@ class ReadExcelCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'app:read-excel-command';
+    protected $signature = 'app:read-excel {--filter=}';
 
     /**
      * The description of the command.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Display the entries of the Excel file in the console, as a table.';
 
     /**
      * Execute the console command.
@@ -28,7 +29,36 @@ class ReadExcelCommand extends Command
      */
     public function handle()
     {
-        //
+        $pathToExcelFile = 'assets/excel-data.xlsx';
+        $filter = $this->option('filter');
+        $rows = SimpleExcelReader::create($pathToExcelFile)->getRows();
+        $columns = ['Host', 'Site Name', 'Expiration Date', 'Support Active', 'Bandwidth', 'Size of Website', 'Owner Email'];
+
+        try {
+            if ($filter == 'active') {
+
+                $rows = $rows->filter(function (array $row) {
+                    return $row['Support Active'] === 'No';
+                });
+
+            }elseif ($filter == 'email') {
+                $rows = $rows->filter(function (array $row) {
+                    return $row['Owner Email'] !== '-' && $row['Owner Email'] !== '';
+                });
+
+            }else{
+                // do nothing
+            }
+
+            $this->table($columns, $rows);
+
+        } catch (\Exception $e) {
+
+            // handle the exception here
+            echo "Error: " . $e->getMessage();
+
+        }
+
     }
 
     /**
